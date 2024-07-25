9e92d7339959ab3bc39258944f15954434b20f84
Pavan Sai <jalsapavan@yahoo.com>
2024-07-25 19:43:34 +0530

    add arrayop

--- arrayop.java
+++ arrayop.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class csort{
+class arrayop{
     public static boolean sort(int[] ar){
         boolean x=true;
         for(int i=01;i<ar.length;i++){
@@ -34,4 +34,19 @@ class csort{
         boolean x=sort(arr);
         System.out.println("Sorted: "+x+"\n size after removing Duplicates"+rd(arr));
     }
+        public void rotate(int[] arr, int k) {
+          k=k%arr.length;
+          ra(arr,0,arr.length-1);
+          ra(arr,0,k-1);
+          ra(arr,k,arr.length-1);
+        }
+        public static void ra(int [] x,int s , int e){
+            while(s<=e){
+                int t=x[s];
+                x[s]=x[e];
+                x[e]=t;
+                e--;
+                s++;
+            }
+        }
 }
